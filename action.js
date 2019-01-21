68bc5ccdae5386a1ef1fb82dff57ebf6cb97c6ce
elbywan <elbywan@hotmail.com>
2019-01-21 16:37:16 +0100

    lower log level

--- action.js
+++ action.js
@@ -3,8 +3,8 @@ const debug = require('debug')
 const { name } = require('./package.json')
 
 // Enable error print
-// debug.enable(name + ':error')
+debug.enable(name + ':error')
 // Uncomment this line to print everything
-debug.enable(name + ':*')
+// debug.enable(name + ':*')
 
 require('./src/index')
